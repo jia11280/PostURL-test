81c178628947ff34d40a8b924d3a9abcd13d3fc5
coolyym <coolyym@coolyymdeair.lan>
2016-10-17 10:38:45 +0800

    Initial Commit

---
+++ PostURL-testTests/PostURL_testTests.m
@@ -0,0 +1,39 @@
+//
+//  PostURL_testTests.m
+//  PostURL-testTests
+//
+//  Created by coolyym on 2016/10/17.
+//  Copyright © 2016年 YYM. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PostURL_testTests : XCTestCase
+
+@end
+
+@implementation PostURL_testTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
